358def02cfbbfaba87c8160f64a1518fe2239881
uzairahmad1434 <uzairahmadkhanmalik@gmail.com>
2024-08-13 18:36:50 +0500

    bgchanger

--- backgroundchanger/src/App.jsx
+++ backgroundchanger/src/App.jsx
@@ -11,49 +11,49 @@ function App() {
     >
       <div className="fixed bottom-10 flex items-center justify-center flex-wrap bg-black p-4 rounded-lg gap-4 ">
         <button
-          className="bg-blue-500 rounded-md px-4 py-2 w-24"
+          className="bg-blue-700 rounded-md px-4 py-2 w-24"
           onClick={() => setColor("blue")}
         >
           blue
         </button>
         <button
-          className="bg-red-500 rounded-md px-4 py-2 w-24"
+          className="bg-red-700 rounded-md px-4 py-2 w-24"
           onClick={() => setColor("red")}
         >
           red
         </button>
         <button
-          className="bg-green-500 rounded-md px-4 py-2 w-24"
+          className="bg-green-700 rounded-md px-4 py-2 w-24"
           onClick={() => setColor("green")}
         >
           green
         </button>
         <button
           className="bg-yellow-500 rounded-md px-4 py-2 w-24"
-          onClick={() => setColor("white")}
+          onClick={() => setColor("yellow")}
         >
           yellow
         </button>
         <button
-          className="bg-pink-500 rounded-md px-4 py-2 w-24"
+          className="bg-pink-300 rounded-md px-4 py-2 w-24"
           onClick={() => setColor("pink")}
         >
           pink
         </button>
         <button
-          className="bg-purple-500 rounded-md px-4 py-2 w-24"
+          className="bg-purple-600 rounded-md px-4 py-2 w-24"
           onClick={() => setColor("purple")}
         >
           purple
         </button>
         <button
-          className="bg-indigo-500 rounded-md px-4 py-2 w-24"
+          className="bg-indigo-600 rounded-md px-4 py-2 w-24"
           onClick={() => setColor("indigo")}
         >
           indigo
         </button>
         <button
-          className="bg-orange-500 rounded-md px-4 py-2 w-24"
+          className="bg-orange-600 rounded-md px-4 py-2 w-24"
           onClick={() => setColor("orange")}
         >
           orange
